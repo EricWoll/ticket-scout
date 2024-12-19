e2fba407404473fdd37e0906b019a488c8febdc1
Eric w <eric.l.woll@gmail.com>
2024-12-19 06:10:27 -0800

    Fixed Page Turner Styles and Updated Event Page. Page Turner number is now bolded to help user see the number better. The Event Page now has more conditional data to allow for events that are missing some data.

--- src/components/page-turner/page-turner.component.tsx
+++ src/components/page-turner/page-turner.component.tsx
@@ -9,14 +9,14 @@ export default function PageTurner({
 }) {
 	return (
 		<div className="w-full flex justify-center">
-			<section className="shadow-main bg-white flex px-5 py-2 gap-4 rounded-md">
+			<section className="shadow-main bg-white flex px-5 py-2 gap-4 rounded-md items-center">
 				<button
 					className="rounded-md px-2 hover:bg-Medium_slate_blue hover:text-white py-1"
 					onClick={prevPage}
 				>
 					Prev
 				</button>
-				<p className="px-5 select-none bg-white shadow-main_inset rounded-md text-Medium_slate_blue">
+				<p className="px-5 select-none bg-white shadow-main_inset rounded-md text-Medium_slate_blue font-bold">
 					{currentPage + 1}
 				</p>
 				<button
--- src/routes/event-page/event-page.route.tsx
+++ src/routes/event-page/event-page.route.tsx
@@ -56,8 +56,8 @@ export default function EventPage() {
 	const date = new Date(cardInfo.dates.start.dateTime);
 
 	return (
-		<main className="m-4">
-			<section className="flex items-start my-4 flex-wrap justify-center">
+		<main className="m-4 grid justify-center">
+			<section className="flex items-start my-4 flex-wrap justify-center gap-2">
 				<div className="p-2 bg-white shadow-main_inset rounded-md max-w-md min-w-20">
 					<Images
 						images={cardInfo.images}
@@ -66,8 +66,8 @@ export default function EventPage() {
 						className="rounded-md"
 					/>
 				</div>
-				<section className="flex flex-col gap-2 ml-6 flex-grow">
-					<section className="flex items-center">
+				<section className="flex flex-col gap-2 flex-grow jus">
+					<section className="flex items-center gap-2">
 						{savedEvent ? (
 							<SavedHeart
 								className="select-none cursor-pointer"
@@ -87,12 +87,14 @@ export default function EventPage() {
 							{cardInfo.classifications[0].subGenre.name}{" "}
 							{cardInfo.classifications[0].genre.name}
 						</p>
-						<p>
-							Age Restrictions:
-							{cardInfo.ageRestrictions.legalAgeEnforced
-								? " Must be of Leagal Age"
-								: " None"}
-						</p>
+						{cardInfo.ageRestrictions?.legalAgeEnforced && (
+							<p>
+								Age Restrictions:
+								{cardInfo.ageRestrictions.legalAgeEnforced
+									? " Must be of Leagal Age"
+									: " None"}
+							</p>
+						)}
 						<p>
 							Event Date:{" "}
 							{date.toLocaleString("en-us", {
@@ -109,57 +111,64 @@ export default function EventPage() {
 								minute: "2-digit",
 							})}
 						</p>
-						<p>
-							Price Range ({cardInfo.priceRanges[0].currency}): $
-							{cardInfo.priceRanges[0].min}
-							{" - "}${cardInfo.priceRanges[0].max}
-						</p>
+						{cardInfo.priceRanges?.length > 0 && (
+							<p>
+								Price Range ({cardInfo.priceRanges[0].currency}
+								): ${cardInfo.priceRanges[0].min}
+								{" - "}${cardInfo.priceRanges[0].max}
+							</p>
+						)}
 					</section>
-					<Link
-						to={cardInfo.url}
-						target="_blank"
-						className="flex flex-row self-stretch gap-1 justify-center px-1 py-2 rounded-md bg-Medium_slate_blue text-white"
-					>
-						Book Tickets
-					</Link>
 				</section>
 			</section>
-			<section className="bg-white shadow-main p-2 grid gap-2 my-4 rounded-md">
-				<h2 className="text-2xl">Presale Information</h2>
-				{cardInfo.sales.presales.length > 0 && (
-					<div className="shadow-main_inset flex flex-col gap-1 p-2 rounded-md">
-						{cardInfo.sales.presales.map((preSale, index) => {
-							return (
-								<div
-									key={index}
-									className="flex flex-wrap gap-12"
-								>
-									<p>
-										Start:{" "}
-										{new Date(
-											preSale.startDateTime
-										).toLocaleString("en-us", {
-											timeZone: cardInfo.dates.timezone,
-											hour: "2-digit",
-											minute: "2-digit",
-										})}
-									</p>
-									<p>
-										End:{" "}
-										{new Date(
-											preSale.endDateTime
-										).toLocaleString("en-us", {
-											timeZone: cardInfo.dates.timezone,
-											hour: "2-digit",
-											minute: "2-digit",
-										})}
-									</p>
-								</div>
-							);
-						})}
-					</div>
-				)}
-			</section>
+			{cardInfo.sales.presales && (
+				<section className="bg-white shadow-main p-2 grid gap-2 my-4 rounded-md">
+					<h2 className="text-2xl">Presale Information</h2>
+					{cardInfo.sales.presales.length > 0 && (
+						<div className="shadow-main_inset flex flex-col p-2 rounded-md">
+							{cardInfo.sales.presales.map((preSale, index) => {
+								return (
+									<div key={index} className="">
+										<p>
+											Start:{" "}
+											{new Date(
+												preSale.startDateTime
+											).toLocaleString("en-us", {
+												month: "short",
+												day: "numeric",
+												year: "numeric",
+												timeZone:
+													cardInfo.dates.timezone,
+												hour: "2-digit",
+												minute: "2-digit",
+											})}
+										</p>
+										<p>
+											End:{" "}
+											{new Date(
+												preSale.endDateTime
+											).toLocaleString("en-us", {
+												month: "short",
+												day: "numeric",
+												year: "numeric",
+												timeZone:
+													cardInfo.dates.timezone,
+												hour: "2-digit",
+												minute: "2-digit",
+											})}
+										</p>
+										{index !=
+											cardInfo.sales.presales.length -
+												1 && (
+											<hr className="h-1 border-0 rounded-md bg-gray-200 my-1" />
+										)}
+									</div>
+								);
+							})}
+						</div>
+					)}
+				</section>
+			)}
 			<section className="bg-white shadow-main p-2 grid gap-2 my-4 rounded-md">
 				<p className="text-2xl">Public Sales</p>
 				<div className="shadow-main_inset flex flex-col gap-1 p-2 rounded-md">
@@ -185,9 +194,18 @@ export default function EventPage() {
 							})}
 						</p>
 					</div>
-					<p>Ticket Limit: {cardInfo.ticketLimit.info}</p>
+					{cardInfo.ticketLimit?.info && (
+						<p>Ticket Limit: {cardInfo.ticketLimit.info}</p>
+					)}
 				</div>
 			</section>
+			<Link
+				to={cardInfo.url}
+				target="_blank"
+				className="w-full flex flex-row self-stretch gap-1 justify-center px-10 py-2 rounded-md bg-Medium_slate_blue text-white max-w-56 mx-auto"
+			>
+				Book Tickets
+			</Link>
 		</main>
 	);
 }
